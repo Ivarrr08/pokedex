fa9245f06c77530b81b4a5b4ecc5be7c90f2c72c
Ivar van de Guchte <ivaryese123@gmail.com>
2018-03-16 11:04:29 +0100

    bug weg

--- Pokemon.php
+++ Pokemon.php
@@ -1,23 +1,22 @@
 <?php
 
-class Pokemon
-{
-	public $name;
-	public $energyType;
-	public $hitpoints;
-	public $health;
-	public $attacks;
-	public $weakness;
-	public $resistance;
+class Pokemon {
+ 	public $name;
+ 	public $energyType;
+ 	public $hitpoints;
+ 	public $health;
+ 	public $attacks = array();
+ 	public $weakness;
+ 	public $resistance;
 
-	public function __construct($name, $energyType, $hitpoints, $health, $attacks, $weakness, $resistance)
-	{
-		$this->name = $name;
-		$this->energyType = $energyType;
-		$this->hitpoints = $hitpoints;
+ 	public function __construct($name, $energyType, $hitpoints, $health, $attacks, $weakness, $resistance)
+ 	{
+ 		$this->name = $name;
+ 		$this->energyType = $energyType;
+ 		$this->hitpoints = $hitpoints;
 		$this->health = $health;
-		$this->attacks = $attacks;
-		$this->weakness = $weakness;
-		$this->resistance = $resistance;
-	}
+ 		$this->attacks = $attacks;
+ 		$this->weakness = $weakness;
+ 		$this->resistance = $resistance;
+ 	}
 }
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-require Pokemon.php;
+require 'Pokemon.php';
